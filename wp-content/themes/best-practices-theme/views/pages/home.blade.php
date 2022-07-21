6d477e59602129e8be5b919fd26166fb36119780
Toheed Ali <toheed2012@hotmail.com>
2022-07-21 18:59:37 +0500

    initial push

--- wp-content/themes/best-practices-theme/views/pages/home.blade.php
+++ wp-content/themes/best-practices-theme/views/pages/home.blade.php
@@ -10,13 +10,13 @@
 					<div class="description">
 						<h1><span class="d-block">Smart Card:</span> The ePaper Label<br> that Saves You Money</h1>
 						<div class="graph-line">
-							<img src="images/bg-line1.png" alt="image-description">
+							<img src="{{public_path('images/bg-line1.png')}}" alt="image-description">
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-6">
 					<div class="image">
-						<img src="images/image1.png" alt="image-description">
+						<img src="{{public_path('images/image1.png')}}" alt="image-description">
 					</div>
 				</div>
 			</div>
@@ -36,25 +36,25 @@
 		<div class="container">
 			<div class="holder">
 				<div class="graph-line">
-					<img src="images/bg-line2.png" alt="image-description">
+					<img src="{{public_path('images/bg-line2.png')}}" alt="image-description">
 				</div>
 				<h2 class="h1">Technology that is Easy<br> to Use and Hard to Resist</h2>
 				<div class="graph-line style01">
-					<img src="images/bg-line3.png" alt="image-description">
+					<img src="{{public_path('images/bg-line3.png')}}" alt="image-description">
 				</div>
 				<div class="col-holder">
 					<div class="col-area">
-						<div class="ico-holder"><img src="images/icon7.svg" alt="image-description" width="57" height="56px"></div>
+						<div class="ico-holder"><img src="{{public_path('images/icon7.svg')}}" alt="image-description" width="57" height="56px"></div>
 						<h2>Innovative Design</h2>
 						<p>Each card is only 3.2mm thick and requires no batteries despite its electronic capabilities.</p>
 					</div>
 					<div class="col-area">
-						<div class="ico-holder"><img src="images/icon8.svg" alt="image-description" width="56" height="56"></div>
+						<div class="ico-holder"><img src="{{public_path('images/icon8.svg')}}" alt="image-description" width="56" height="56"></div>
 						<h2>Fast Data Transfer</h2>
 						<p>Smart Card is readable at the high speed of 120m per minute with no slowdown.</p>
 					</div>
 					<div class="col-area">
-						<div class="ico-holder"><img src="images/icon9.svg" alt="image-description" width="55" height="55"></div>
+						<div class="ico-holder"><img src="{{public_path('images/icon9.svg')}}" alt="image-description" width="55" height="55"></div>
 						<h2>Smart Device Compatibility</h2>
 						<p>Near Field Communication (NFC) technology enables you to seamlessly overwrite the display with the desired information from your mobile device.</p>
 					</div>
@@ -68,7 +68,7 @@
 				<div class="col-lg-6">
 					<div class="video-box">
 						<a href="#">
-							<img src="images/image2.jpg" alt="image-description">
+							<img src="{{public_path('images/image2.jpg')}}" alt="image-description">
 							<span class="btn-play"></span>
 						</a>
 					</div>
@@ -87,7 +87,7 @@
 			<div class="holder">
 				<div class="head">
 					<div class="graph-line">
-						<img src="images/bg-line4.png" alt="image-description">
+						<img src="{{public_path('images/bg-line4.png')}}" alt="image-description">
 					</div>
 					<h2 class="h1">Designed for Many Economic and Industrial Environments</h2>
 					<p>The ability of Smart Card to display different types of information <br>enables the device to operate in multiple commercial settings.</p>
@@ -95,42 +95,42 @@
 				<div class="topics-blocks">
 					<div class="topic-block">
 						<div class="ico-holder">
-							<img src="images/icon1.svg" alt="image-description" width="58" height="58">
+							<img src="{{public_path('images/icon1.svg')}}" alt="image-description" width="58" height="58">
 						</div>
 						<h2>Manufacturing</h2>
 						<p>Factories can distribute paperless, real-time production orders to their workers, eliminating handwriting errors, reducing waste, and improving operational efficiency.</p>
 					</div>
 					<div class="topic-block">
 						<div class="ico-holder">
-							<img src="images/icon2.svg" alt="image-description" width="55" height="56">
+							<img src="{{public_path('images/icon2.svg')}}" alt="image-description" width="55" height="56">
 						</div>
 						<h2>Distribution</h2>
 						<p>Compatible with our Pick-to-Light systems, Smart Card makes it easier for warehouse staff to navigate the transfer of inventory from one point to another.</p>
 					</div>
 					<div class="topic-block">
 						<div class="ico-holder">
-							<img src="images/icon3.svg" alt="image-description" width="54" height="54">
+							<img src="{{public_path('images/icon3.svg')}}" alt="image-description" width="54" height="54">
 						</div>
 						<h2>Food Service</h2>
 						<p>Restaurants use our RFID tags to quickly process billing information and receive payment from their customers at greater speeds.</p>
 					</div>
 					<div class="topic-block">
 						<div class="ico-holder">
-							<img src="images/icon4.svg" alt="image-description" width="56" height="56">
+							<img src="{{public_path('images/icon4.svg')}}" alt="image-description" width="56" height="56">
 						</div>
 						<h2>Health Care</h2>
 						<p>Smart Card allows medical personnel to safely handle patient information and equipment to decrease the risks of leaking personal information.</p>
 					</div>
 					<div class="topic-block">
 						<div class="ico-holder">
-							<img src="images/icon5.svg" alt="image-description" width="56" height="56">
+							<img src="{{public_path('images/icon5.svg')}}" alt="image-description" width="56" height="56">
 						</div>
 						<h2>Maintenance</h2>
 						<p>Facilities install Smart Cards on their walls to log the work history of cleaning staff with an accuracy nearly impossible to duplicate with paper.</p>
 					</div>
 					<div class="topic-block">
 						<div class="ico-holder">
-							<img src="images/icon6.svg" alt="image-description" width="55" height="55">
+							<img src="{{public_path('images/icon6.svg')}}" alt="image-description" width="55" height="55">
 						</div>
 						<h2>Businesses/Organizations</h2>
 						<p>Smart Card excels as a reception system that records the personal information of visitors, grants them access to the building, and erases their information afterwards</p>
@@ -146,7 +146,7 @@
 					<div class="column column-head">
 						<div class="column-wrap">
 							<div class="graph-line">
-								<img src="images/bg-line5.png" alt="image-description">
+								<img src="{{public_path('images/bg-line5.png')}}" alt="image-description">
 							</div>
 							<div class="head">
 								<h2 class="h1">Visible RFID Smart Card Specifications</h2>
@@ -173,7 +173,7 @@
 						<div class="column-wrap">
 							<div class="head">
 								<div class="image">
-									<img src="images/image3.png" alt="image-description">
+									<img src="{{public_path('images/image3.png')}}" alt="image-description">
 								</div>
 							</div>
 							<div class="data-holder">
@@ -198,7 +198,7 @@
 						<div class="column-wrap">
 							<div class="head">
 								<div class="image">
-									<img src="images/image4.png" alt="image-description">
+									<img src="{{public_path('images/image4.png')}}" alt="image-description">
 								</div>
 							</div>
 							<div class="data-holder">
@@ -229,10 +229,10 @@
 				<div class="col-lg-7">
 					<div class="description">
 						<div class="graph-line">
-							<img src="images/bg-line6.png" alt="image-description">
+							<img src="{{public_path('images/bg-line6.png')}}" alt="image-description">
 						</div>
 						<div class="graph-line add">
-							<img src="images/bg-line7.png" alt="image-description">
+							<img src="{{public_path('images/bg-line7.png')}}" alt="image-description">
 						</div>
 						<h2 class="h1">Featured Case Studies</h2>
 						<p>OMRON ASO, a product manufacturer in Japan, sought a paperless way to optimize parts traceability and product management on all levels. Watch how Smart Card seamlessly replaced instructions, paper Kanban, and tags to transform the manufacturing process at OMRON for the better.</p>
@@ -242,7 +242,7 @@
 				<div class="col-lg-5">
 					<div class="video-box">
 						<a href="#">
-							<img src="images/image5.jpg" alt="image-description">
+							<img src="{{public_path('images/image5.jpg')}}" alt="image-description">
 							<span class="btn-play"></span>
 						</a>
 					</div>
@@ -268,26 +268,26 @@
 				<div class="col-lg-6">
 					<div class="description">
 						<div class="graph-line">
-							<img src="images/bg-line8.png" alt="image-description">
+							<img src="{{public_path('images/bg-line8.png')}}" alt="image-description">
 						</div>
 						<div class="graph-line add">
-							<img src="images/bg-line9.png" alt="image-description">
+							<img src="{{public_path('images/bg-line9.png')}}" alt="image-description">
 						</div>
 						<h2 class="h1">Try Smart Card from Your Mobile Device</h2>
 						<p>Don’t feel pressured into purchasing our product before testing it out. Download our free demo apps and witness first-hand what Smart Card has to offer.</p>
 						<div class="app-btn-holder">
-							<a href="#"><img src="images/app-btn.png" alt="image-description"></a>
-							<a href="#"><img src="images/google-btn.png" alt="image-description"></a>
+							<a href="#"><img src="{{public_path('images/app-btn.png')}}" alt="image-description"></a>
+							<a href="#"><img src="{{public_path('images/google-btn.png')}}" alt="image-description"></a>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-5">
 					<div class="mob-image">
 						<div class="image">
-							<img src="images/image6.png" alt="image-description">
+							<img src="{{public_path('images/image6.png')}}" alt="image-description">
 						</div>
 						<div class="card-image">
-							<img src="images/image4.png" alt="image-description">
+							<img src="{{public_path('images/image4.png')}}" alt="image-description">
 						</div>
 					</div>
 				</div>
@@ -298,7 +298,7 @@
 					<p>Please fill out the following contact form. A member of our team will review your request and reach out to you shortly.</p>
 				</div>
 				<div class="form-area">
-					<img src="images/form-placeholder.jpg">
+					<img src="{{public_path('images/form-placeholder.jpg')}}">
 				</div>
 			</div>
 		</div>
