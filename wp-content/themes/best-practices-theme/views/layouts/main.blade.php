0b028da546880fa42151f13f32d4689d8a18a59e
toheed-zz <toheed2012@hotmail.com>
2022-07-22 00:22:30 +0500

    css changes

--- wp-content/themes/best-practices-theme/views/layouts/main.blade.php
+++ wp-content/themes/best-practices-theme/views/layouts/main.blade.php
@@ -9,6 +9,7 @@
             {!! wp_head() !!}
             <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;0,900;1,400&display=swap" rel="stylesheet">
             <link rel="stylesheet" href="{{mix('main.css')}}">
+            <link rel="stylesheet" href="{{public_path('custom.css')}}">
         @show
     </head>
 
