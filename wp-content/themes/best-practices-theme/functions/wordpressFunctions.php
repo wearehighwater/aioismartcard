c8508843e81d81c8e263fe0ff0462dbbf6cf1d86
toheed-zz <toheed2012@hotmail.com>
2022-07-25 17:07:50 +0500

    css changes

--- wp-content/themes/best-practices-theme/functions/wordpressFunctions.php
+++ wp-content/themes/best-practices-theme/functions/wordpressFunctions.php
@@ -13,6 +13,7 @@ if ( function_exists('acf_add_options_page') ) {
     acf_add_options_page();
     acf_add_options_sub_page('Header');
     acf_add_options_sub_page('Footer');
+    acf_add_options_sub_page('Product Table');
 }
 
 if ( ! function_exists( 'theme_setup' ) ) :
