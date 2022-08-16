f5f4e074026f9db277a41d73c562717325a09e30
toheed-zz <toheed2012@hotmail.com>
2022-08-16 07:06:32 -0700

    css changes

--- wp-content/themes/best-practices-theme/views/pages/how-it-works.blade.php
+++ wp-content/themes/best-practices-theme/views/pages/how-it-works.blade.php
@@ -9,7 +9,7 @@
                         <h1><span class="d-block">{!!get_field('visual_head')!!}</h1>
                         <p>{!!get_field('visual_subhead')!!}</p>
                         <div class="graph-line">
-                            <img src="{{public_path('images/how-bg-line1.svg')}}" width="694" height="575" alt="image-description">
+                            <img src="{{public_path('images/how-bg-line1.svg')}}" width="694" height="575" alt="">
                             <div class="pulsating-circle">
                                 <div class="circle" style="animation-delay: -3s"></div>
                                 <div class="circle" style="animation-delay: -2s"></div>
@@ -49,7 +49,7 @@
                 <div class="col-lg-7">
                     <div class="description">
                         <div class="graph-line">
-                            <img src="{{public_path('images/how-bg-line2.svg')}}" width="688" height="357" alt="image-description">
+                            <img src="{{public_path('images/how-bg-line2.svg')}}" width="688" height="357" alt="">
                             <div class="pulsating-circle">
                                 <div class="circle" style="animation-delay: -3s"></div>
                                 <div class="circle" style="animation-delay: -2s"></div>
@@ -58,7 +58,7 @@
                             </div>
                         </div>
                         <div class="graph-line add">
-                            <img src="{{public_path('images/how-bg-line3.svg')}}" width="703" height="692" alt="image-description">
+                            <img src="{{public_path('images/how-bg-line3.svg')}}" width="703" height="692" alt="">
                             <div class="pulsating-circle add">
                                 <div class="circle" style="animation-delay: -3s"></div>
                                 <div class="circle" style="animation-delay: -2s"></div>
@@ -83,7 +83,7 @@
             <div class="row justify-content-center">
                 <div class="col-lg-8 col-xl-6">
                     <div class="ico-holder">
-                        <img src="{{get_field('how_it_works_section_3_image')}}" alt="image-description" width="150" height="150">
+                        <img src="{{get_field('how_it_works_section_3_image')}}" alt="" width="150" height="150">
                     </div>
                     <h2 class="h1">{!!get_field('how_it_works_section_3_title')!!}</h2>
                     <p>{!!get_field('how_it_works_section_3_content')!!}</p>
@@ -96,13 +96,13 @@
             <div class="row align-items-center">
                 <div class="col-lg-5">
                     <div class="image">
-                        <img src="{{get_field('how_it_works_section_4_image')}}" alt="image-description">
+                        <img src="{{get_field('how_it_works_section_4_image')}}" alt="">
                     </div>
                 </div>
                 <div class="col-lg-7">
                     <div class="description">
                         <div class="graph-line">
-                            <img src="{{public_path('images/how-bg-line4.svg')}}" width="375" height="322" alt="image-description">
+                            <img src="{{public_path('images/how-bg-line4.svg')}}" width="375" height="322" alt="">
                             <div class="pulsating-circle">
                                 <div class="circle" style="animation-delay: -3s"></div>
                                 <div class="circle" style="animation-delay: -2s"></div>
@@ -123,7 +123,7 @@
                 <div class="col-lg-6">
                     <div class="description">
                         <div class="graph-line">
-                            <img src="{{public_path('images/how-bg-line5.svg')}}" width="968" height="353" alt="image-description">
+                            <img src="{{public_path('images/how-bg-line5.svg')}}" width="968" height="353" alt="">
                             <div class="pulsating-circle bg-white">
                                 <div class="circle" style="animation-delay: -3s"></div>
                                 <div class="circle" style="animation-delay: -2s"></div>
@@ -137,7 +137,7 @@
                 </div>
                 <div class="col-lg-6">
                     <div class="image">
-                        <img src="{{get_field('how_it_works_section_5_image')}}" alt="image-description">
+                        <img src="{{get_field('how_it_works_section_5_image')}}" alt="">
                     </div>
                 </div>
             </div>
